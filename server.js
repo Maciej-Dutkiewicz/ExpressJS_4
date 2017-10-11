3d91bb514aca84a5b214ae19327c40f263746f0b
Maciek <m.p.dutkiewicz@gmail.com>
2017-10-11 17:58:33 +0200

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req,res) {
+	res.render('start.pug')
+});
+
+app.get('/auth/google', function(req,res) {
+	res.render('log.pug');
+});
+
+app.get('/login', function(req,res) {
+	res.render('login.pug');
+});
+
+app.listen(3000);
